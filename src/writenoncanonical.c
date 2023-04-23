09b6df2768353274693ee3a79d6ec413ceac1cc3
dvalinn <tiago.andre.amorim@gmail.com>
2023-04-23 18:29:20 +0100

    added rule to allow socat serial ports

--- src/writenoncanonical.c
+++ src/writenoncanonical.c
@@ -24,7 +24,8 @@ int main(int argc, char** argv) {
 
     if ((argc < 2) ||
         ((strcmp("/dev/ttyS0", argv[1]) != 0) &&
-         (strcmp("/dev/ttyS1", argv[1]) != 0))) {
+         (strcmp("/dev/ttyS1", argv[1]) != 0) &&
+         (strcmp("/dev/ttyS10", argv[1]) != 0))) {
         printf("Usage:\tnserial SerialPort\n\tex: nserial /dev/ttyS1\n");
         exit(1);
     }
