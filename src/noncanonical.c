6ccbe83c7aaf3d4be7981d8d7a8dcf032c843b8b
dvalinn <tiago.andre.amorim@gmail.com>
2023-04-23 18:56:49 +0100

    bugfixes

--- src/noncanonical.c
+++ src/noncanonical.c
@@ -71,8 +71,8 @@ int main(int argc, char** argv) {
     printf("New termios structure set\n");
 
     while (STOP == FALSE) {       /* loop for input */
-        res = read(fd, buf, 255); /* returns after 5 chars have been input */
-        buf[res] = 0;             /* so we can printf... */
+        res = read(fd, buf, 255); /* returns after 255 chars have been input */
+        buf[res] = '\0';          /* so we can printf... */
         printf(":%s:%d\n", buf, res);
         if (buf[0] == 'z')
             STOP = TRUE;
