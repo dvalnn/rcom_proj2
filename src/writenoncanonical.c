7d6874bb8428d174ef45f84e8ed20a3c625f4302
dvalinn <tiago.andre.amorim@gmail.com>
2023-04-24 09:33:36 +0100

    removed unnecessary if conditions

--- src/writenoncanonical.c
+++ src/writenoncanonical.c
@@ -11,7 +11,6 @@
 #include "log.h"
 
 #define BAUDRATE B38400
-#define MODEMDEVICE "/dev/ttyS1"
 #define _POSIX_SOURCE 1 /* POSIX compliant source */
 #define FALSE 0
 #define TRUE 1
@@ -27,10 +26,7 @@ int main(int argc, char** argv) {
     char buf[BUF_SIZE];
     // int i, sum = 0, speed = 0;
 
-    if ((argc < 2) ||
-        ((strcmp("/dev/ttyS0", argv[1]) != 0) &&
-         (strcmp("/dev/ttyS1", argv[1]) != 0) &&
-         (strcmp("/dev/ttyS10", argv[1]) != 0))) {
+    if (argc < 2) {
         printf("Usage:\tnserial SerialPort\n\tex: nserial /dev/ttyS1\n");
         exit(1);
     }
