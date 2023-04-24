179917e1befbf95f35dea99df309c480faf71009
dvalinn <tiago.andre.amorim@gmail.com>
2023-04-24 10:24:45 +0100

    SET message state machine and handler

--- src/noncanonical.c
+++ src/noncanonical.c
@@ -9,6 +9,7 @@
 #include <stdlib.h>
 
 #include "log.h"
+#include "macros.h"
 
 #define BAUDRATE B38400
 #define _POSIX_SOURCE 1 /* POSIX compliant source */
@@ -16,6 +17,86 @@
 #define TRUE 1
 
 volatile int STOP = FALSE;
+
+#pragma region states
+typedef enum states {
+    st_START,
+    st_FLAG_RCV,
+    st_A_RCV,
+    st_C_RCV,
+    st_BCC_OK,
+    st_STOP
+} states;
+
+states state_update(states cur_state, unsigned char rcved) {
+    states new_state = cur_state;
+
+    switch (cur_state) {
+        case st_START:
+            if (rcved == F)
+                new_state = st_FLAG_RCV;
+            break;
+
+        case st_FLAG_RCV:
+            if (rcved == A)
+                new_state = st_A_RCV;
+            else if (rcved != F)
+                new_state = st_START;
+            break;
+
+        case st_A_RCV:
+            if (rcved == C)
+                new_state = st_C_RCV;
+            if (rcved == F)
+                new_state = st_FLAG_RCV;
+            else
+                new_state = st_START;
+            break;
+
+        case st_C_RCV:
+            if (rcved == BCC)
+                new_state = st_BCC_OK;
+            if (rcved == F)
+                new_state = st_FLAG_RCV;
+            else
+                new_state = st_START;
+            break;
+
+        case st_BCC_OK:
+            if (rcved == F)
+                new_state = st_STOP;
+            else
+                new_state == st_START;
+            break;
+
+        case st_STOP:
+            break;
+    }
+
+    return new_state;
+}
+
+void state_handler(int fd) {
+    states cur_state = st_START;
+
+    unsigned char flag;
+
+    while (cur_state != st_STOP) {
+        LOG("Current State: %d\n", cur_state);
+        short byte = read(fd, flag, 1);
+        if (byte != 1)
+            ERROR("Could not read char from file descriptor\n");
+        cur_state = state_update(cur_state, flag);
+    }
+    write(fd, "WAH", sizeof("WAH"));
+}
+
+#undef F
+#undef A
+#undef C
+#undef BCC
+
+#pragma endregion
 
 int main(int argc, char** argv) {
     // int fd, c, res;
@@ -69,7 +150,11 @@ int main(int argc, char** argv) {
     }
 
     printf("New termios structure set.\n");
-    printf("Waiting on a transmission.\n");
+    printf("Waiting on SET transmission.\n");
+
+    state_handler(fd);
+
+    LOG("SET successfull\n");
 
     while (STOP == FALSE) {       /* loop for input */
         res = read(fd, buf, 255); /* returns after 255 chars have been input */
